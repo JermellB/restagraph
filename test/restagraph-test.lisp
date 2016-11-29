53d5defd39d1133d2d91a8f20611280601acbca6
James Fleming <james@electronic-quill.net>
2016-11-29 19:16:48 +0100

    Add a basic test

--- test/restagraph-test.lisp
+++ test/restagraph-test.lisp
@@ -10,3 +10,21 @@
 
 (fiveam:def-suite main)
 (fiveam:in-suite main)
+
+(fiveam:test
+  rg-schema
+  "Check the core schema operations"
+  (let ((schema (make-hash-table :test 'equal)))
+  ;; Add a class to the schema
+  (fiveam:is (hash-table-p (restagraph::add-class-to-schema schema "foo")))
+  ;; Confirm the class' presence in the schema
+  (fiveam:is (hash-table-p (restagraph::get-class-from-schema-by-name schema "foo")))
+  ;; Add another class
+  (fiveam:is (hash-table-p (restagraph::add-class-to-schema schema "bar")))
+  ;; Confirm that we now have exactly the two classes in the schema that we expected
+  (fiveam:is (hash-table-p (restagraph::get-class-from-schema-by-name schema "foo")))
+  (fiveam:is (hash-table-p (restagraph::get-class-from-schema-by-name schema "bar")))
+  ;; Add a relationship between the classes
+  (fiveam:is (listp (restagraph::add-class-relationship-to-schema schema "foo" "is-a" "bar")))
+  ;; Confirm that the relationship is present
+  ))
