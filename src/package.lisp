1e5ee9f664aa903ae7cefd766cc7153dc84fb732
James Fleming <james@electronic-quill.net>
2017-04-06 08:28:09 +0200

    Export a batch of methods

--- src/package.lisp
+++ src/package.lisp
@@ -11,4 +11,23 @@
     integrity-error
     client-error
     message
+    ;; Methods
+    get-resource-defs-from-db
+    get-resource-attributes-from-db
+    relationship-valid-p
+    enforce-db-schema
+    dependent-resource-p
+    store-resource
+    store-dependent-resource
+    move-dependent-resource
+    get-resources
+    get-dependent-resources
+    get-dependen-relationships-for-type
+    dependent-relationship-p
+    create-relationship-by-path
+    get-resources-with-relationship
+    check-relationship-by-path
+    delete-relationship-by-path
+    get-relationship-attrs
+    delete-resource-by-path
     ))
