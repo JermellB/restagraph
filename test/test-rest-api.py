0225dff1449e44efcb76ccb766d817b0a9d5d42a
James Fleming <james@electronic-quill.net>
2017-04-20 19:08:11 +0200

    Add Apache 2.0 license

--- test/test-rest-api.py
+++ test/test-rest-api.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 
 # Test package for Restagraph's REST API.
+
+
+#   Copyright 2017 James Fleming <james@electronic-quill.net>
 #
-# Written in Python because that's the language I expect most users to implement
-# their code in.
-# Using Python3 because it's about time we all moved forward, and Docker means
-# we no longer need to stick with obsolete stuff.
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
 
 # Included batteries
