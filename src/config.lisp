d1f31fe75c0aeb40b2a438ef4fd22ce355f151c0
James Fleming <james@electronic-quill.net>
2020-07-13 19:44:10 +0200

    Added necessary trailing slash to files-temp-location in config.lisp

--- src/config.lisp
+++ src/config.lisp
@@ -18,7 +18,7 @@
     :api-uri-base "/raw/v1"
     :schema-uri-base "/schema/v1"
     :files-uri-base "/files/v1"
-    :files-temp-location "/tmp/restagraph-files-tmp"
+    :files-temp-location "/tmp/restagraph-files-tmp/"
     :files-location "/tmp/restagraph-files"))
 
 (setf *loglevel* :info)
