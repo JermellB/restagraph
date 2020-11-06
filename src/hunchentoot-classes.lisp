44a38eb38445b9dbed8bdd689ca10fd6113de4b1
James Fleming <james@electronic-quill.net>
2020-11-06 17:06:23 +0100

    Improve documentation in (defclass restagraph-acceptor)

--- src/hunchentoot-classes.lisp
+++ src/hunchentoot-classes.lisp
@@ -28,11 +28,13 @@
                    :documentation "Base URI on which the files API is to be presented.")
    (files-location :initarg :files-location
                    :reader files-location
-                   :initform (error "files-location is required"))
+                   :initform (error "files-location is required")
+                   :documentation "Parent directory under which file objects are to be stored.")
    (schema :initarg :schema
            :accessor schema
            :initform (make-hash-table :test #'equal)
            :documentation "What to use for schema actions. Valid options are a hash-table or the datastore object, as methods are specialised on both."))
-  ;; Class defaults
+  ;; Class defaults for initalising the superclass
   (:default-initargs :address "127.0.0.1")
+  ;; Class documentation
   (:documentation "Customised Hunchentoot acceptor, subclassed from tbnl:easy-acceptor. Carries additional configuration data for the site."))
